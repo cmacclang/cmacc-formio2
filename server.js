683f88af5a9bd27e8a766e9c4d43fe127ebe9ece
Willem Veelenturf <wveelenturf@bol.com>
2017-10-27 11:01:25 +0200

    init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+//const expressSession = require('express-session');
+//const expressHandlebars = require('express-handlebars');
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use('/lib/formiojs', express.static('node_modules/formiojs/dist'));
+app.use('/lib/bootstrap', express.static('node_modules/bootstrap/dist'));
+
+app.use('/forms', express.static('forms'));
+app.use('/', express.static('html'));
+
+//app.use(require('./routes/testRoute'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
